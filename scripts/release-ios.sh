7391087edd1ef5be300509c953e34196fffd3435
Maartje Eyskens <meyskens@me.com>
2017-08-23 22:04:39 +0200

    fix location of fix-cert.sh

--- scripts/release-ios.sh
+++ scripts/release-ios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export FL_PROJECT_SIGNING_FORCE_UPGRADE=true
-./fix-cert.sh
+../scripts/fix-cert.sh
 /usr/libexec/PlistBuddy -c "Add :ITSAppUsesNonExemptEncryption bool false" ./platforms/ios/ReadMyAge/ReadMyAge-Info.plist
 fastlane add_plugin update_project_codesigning
 fastlane setup
