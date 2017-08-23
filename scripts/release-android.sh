ec18b5a956df4a6d19e7821a54ffb452430b5772
Maartje Eyskens <meyskens@me.com>
2017-08-23 14:32:17 +0200

    release android beta

--- scripts/release-android.sh
+++ scripts/release-android.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://$CI_USER_TOKEN@github.com/meyskens/playstore-key.git
 cordova build android --release -- --keystore=./playstore-key/my-release-key.keystore --storePassword=$KEYSTORE_PASS --alias=alias_name --password=$KEYSTORE_PASS
+fastlane beta-android
