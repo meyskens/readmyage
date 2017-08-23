728682e1b77a940f5b984abf69fd0f39beffa2fa
Maartje Eyskens <meyskens@me.com>
2017-08-23 13:48:59 +0200

    add signing key

--- scripts/release-android.sh
+++ scripts/release-android.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-ionic cordova build android --release
+git clone https://$CI_USER_TOKEN@github.com/meyskens/playstore-key.git
+cordova build android --release -- --keystore=./playstore-key/my-release-key.keystore --storePassword=$KEYSTORE_PASS --alias=alias_name --password=$KEYSTORE_PASS
