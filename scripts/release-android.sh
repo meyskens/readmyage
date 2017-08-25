caceb3791cd69a9a633a78a8a301a1cfa0841de7
Maartje Eyskens <meyskens@me.com>
2017-08-25 15:20:00 +0200

    try double building android

--- scripts/release-android.sh
+++ scripts/release-android.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://$CI_USER_TOKEN@github.com/meyskens/playstore-key.git
+ionic cordova build android --release
 cordova build android --release -- --keystore=./playstore-key/my-release-key.keystore --storePassword=$KEYSTORE_PASS --alias=alias_name --password=$KEYSTORE_PASS
 fastlane betaandroid
