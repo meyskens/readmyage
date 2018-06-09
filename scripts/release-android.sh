746e1fdbaa641fdedb7d77ab6528bb5e2fd376c0
Maartje Eyskens <meyskens@me.com>
2018-06-09 10:42:34 +0200

    patch

--- scripts/release-android.sh
+++ scripts/release-android.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp ../patch/build.gradle ./platforms/android/build.gradle
 git clone https://$CI_USER_TOKEN@github.com/meyskens/playstore-key.git
 ionic cordova build android --release
 cordova build android --release -- --keystore=./playstore-key/my-release-key.keystore --storePassword=$KEYSTORE_PASS --alias=alias_name --password=$KEYSTORE_PASS
