707ba6cd428b8613ec7ac323ce17e91bfb51292b
Maartje Eyskens <meyskens@me.com>
2018-06-10 20:29:07 +0200

    do not install bundler

--- scripts/release-ios.sh
+++ scripts/release-ios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ]; then exit 0; fi
 export FL_PROJECT_SIGNING_FORCE_UPGRADE=true
-sudo gem install bundler
+#sudo gem install bundler
 ../scripts/fix-cert.sh
 /usr/libexec/PlistBuddy -c "Add :ITSAppUsesNonExemptEncryption bool false" ./platforms/ios/ReadMyAge/ReadMyAge-Info.plist
 fastlane add_plugin update_project_codesigning
